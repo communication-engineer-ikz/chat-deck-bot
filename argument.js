3eda958ff11f023c240a56406f93c88a99065633
ikz <75650546+communication-engineer-ikz@users.noreply.github.com>
2021-01-31 23:22:23 +0900

    アクセストークンが記載されているファイルを削除

--- argument.js
+++
@@ -1,4 +0,0 @@
-function setAccessToken(){
-  const ACCESS_TOKEN = 'bCJimqMb6ljCw7uNMMQ0rKLerVkxDD7Sh1zpX/958PFimv0evR7MyP7rHz8AMAx6n1rs+zvghW6Oekt+TVHiXAAuL5h89kwh7smgc3bYAkk/FuWfEdMwHMpLgSjElFVhddGm45LswEjcAKZulW2ZtAdB04t89/1O/w1cDnyilFU='; //step1 Channel access tokenをペースト
-  return ACCESS_TOKEN;
-}
